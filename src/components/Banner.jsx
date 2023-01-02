ece9871f3069715b8f2303ed9d4bf8d8271162a5
federicocaballero <fedecaballero1234@gmail.com>
2023-01-02 12:45:22 -0300

    Primeros estilos en Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,18 +3,22 @@ import React from "react"
 const Banner = () => {
     return(
         <>
-        <div class="text-center">
+        <div class="text-center banner-component p-4">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-8 p-4">
                 <div className="w-100 h-100 d-flex flex-column align-items-center justify-content-center ">
-                    <h6>Hello world!</h6>
+                    <p>Hello world!</p>
                     <p>My name is Federico Caballero. <br/>
                     I'm junior frontend developer</p>
-                    <button type="button" class="btn btn-outline-dark text-uppercase">contact me</button>
+                    <div class=" d-flex justify-content-evenly gap-4">
+                        <button type="button" class="btn btn-outline text-uppercase">contact me</button>
+                        <button type="button" class="btn btn-outline text-uppercase">projects</button>
+                    </div>
+                    
                 </div>
             </div>
-            <div class="col-md-4 bg-info">
-            <img src="https://economis.com.ar/wp-content/uploads/2022/12/messi-copa.jpg" class="img-fluid" alt="img"/>
+            <div class="col-md-4">
+                <img src="portfolio\public\logo192.png" class="w-100 h-100" alt="" />
             </div>
         </div>
         </div>
