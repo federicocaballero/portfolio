6f827f2dfd6ee7b8ae2c72603de20a6183f38292
federicocaballero <fedecaballero1234@gmail.com>
2023-01-05 12:55:48 -0300

    Nuevo componente Contact y agrego Bootstrap Icons

---
+++ src/components/Contact.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Contact = () => {
+    return(
+        <>
+            <div>
+                <p>Si tenes una oferta de trabajo, estás interesado en que trabajemos juntos o simplemente queres saludarme, contáctame aquí</p>
+
+                <div class="container text-center">
+                <div class="row">
+                    <a href="">
+
+                    </a>
+                    <a href="">
+
+                    </a>
+                    <a href="mailto:fedecaballero1234@gmail.com">Aquí el texto que quieras</a>
+                    <div class="col">
+                    
+                    </div>
+                </div>
+                </div>
+            </div>
+        </>
+    )
+}
