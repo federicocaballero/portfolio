5d9d5765714958f0eb3ced36da3a024fe5829098
Federico Caballero <fedecaballero1234@gmail.com>
2023-01-24 10:09:10 -0300

    Estilos a las imagenes añadidas

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,15 +4,15 @@ const Projects = ({title, description, url, urlRepo, img}) => {
     return(
         <>
         <div class="row">
-            <div class="col-md pt-3">
+            <div class="col-md-6 pt-3">
             <h3 className="text-uppercase title">{title}</h3>
             <p class="projects-text">{description}
             </p>    
             <a class="link-projects" target="blank" href={url}>Go to website</a>
             <a class="link-projects ms-5" target="blank" href={urlRepo}>View code</a>
             </div>  
-            <div class="col-md">
-                <img src={img} alt="" />
+            <div class="col-md-6">
+                <img src={img} class="img-fluid img-project" alt="img-project" />
             </div>
         </div>  
         
