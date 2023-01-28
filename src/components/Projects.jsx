e96e6cb474f602925c409b0c1be61ee51934955c
Federico Caballero <fedecaballero1234@gmail.com>
2023-01-28 13:04:37 -0300

    Cambio de diseño en Componentes de Proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Projects = ({title, description, url, urlRepo, img}) => {
     return(
         <>
-        <div class="row">
+        {/* <div class="row">
             <div class="col-md-6 pt-3">
             <h3 className="text-uppercase title">{title}</h3>
             <p class="projects-text">{description}
@@ -15,7 +15,19 @@ const Projects = ({title, description, url, urlRepo, img}) => {
                 <img src={img} class="img-fluid img-project" alt="img-project" />
             </div>
         </div>  
-        
+         */}
+        {/* NUEVO PROYECTOS */}
+
+        <div>
+            
+                <img src={img} class="img-fluid img-project" alt="img-project" />
+                <div>
+                    <h3 className="text-uppercase title">{title}</h3>
+                    <p class="projects-text">{description}  </p> 
+                    <a class="link-projects" target="blank" href={url}>Go to website</a>
+                    <a class="link-projects ms-5" target="blank" href={urlRepo}>View code</a>
+                </div>           
+        </div>
         </>
     )
 }
--- src/containers/ProjectsContainer.jsx
+++ src/containers/ProjectsContainer.jsx
@@ -5,28 +5,52 @@ import birreria from "../assets/images/birreria.png"
 import travelAgency from "../assets/images/travel-agency.png"
 import portfolio from "../assets/images/portfolio.png"
 const ProjectsContainer = () => {
+ 
     return(
 
         <>    
         <div class="wave-top1" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" class="wave-top2"><path d="M-0.00,49.98 C150.00,149.99 271.49,-49.98 500.00,49.98 L500.00,-0.00 L-0.00,-0.00 Z" class="wave-top3"></path></svg></div>
         <div class="wave-bottom1" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" class="wave-bottom2"><path d="M-0.00,49.98 C150.00,149.99 271.49,-49.98 500.00,49.98 L500.00,-0.00 L-0.00,-0.00 Z" class="wave-bottom3"></path></svg></div>   
-        <div id="carouselExampleSlidesOnly" class="carousel  slide ps-5 pt-0 pe-5 pb-5 m-0 skills-component" data-bs-ride="carousel">
-        <h3 id="projects" className="text-uppercase title text-center">proyectos</h3>
+        {/* <div id="carouselExampleSlidesOnly" class="carousel  slide ps-5 pt-0 pe-5 pb-5 m-0 skills-component" data-bs-ride="carousel"> */}
+        
+        {/*         
         <div class="carousel-inner mt-4">
-            <div class="carousel-item active">
+            <div class="carousel-item">
                 <Projects title={'Birreria'} description={'Este proyecto muestra las opciones y locales de un local de cerveza artesanal. Muestra las sucursales y marcas que sirven.'} url={'https://frontend-birreria.web.app/'} urlRepo={'https://github.com/federicocaballero/Birreria-FrontEnd.git'} img={birreria}/>
             </div>
             <div class="carousel-item">
                 <Projects title={'Travel Agency'} description={'Este pagina muestra los distintos destinos y ofertas de una agencia de viajes'} url={''} urlRepo={'https://github.com/federicocaballero/Travel-Agency-Home.git'} img={travelAgency} />
             </div>
-            <div class="carousel-item active">
+            <div class="carousel-item ">
                 <Projects title={'Blog de café'} description={'Blog de sobre cafe con distintas paginas para navegar (Contacto, Cursos, Nosotros)'} url={'https://blogdecafetp.netlify.app/index.html'} urlRepo={'https://github.com/federicocaballero/blog-de-cafe.git'} img={blogCafe}/>
             </div>
-            <div class="carousel-item">
+            <div class="carousel-item active">
                 <Projects title={'Portfolio'} description={'Mi propio portfolio'} url={''} urlRepo={'https://github.com/federicocaballero/portfolio.git'} img={portfolio} />
             </div>
-        </div>
-        </div>
+        </div> */}  
+        {/* </div> */}
+        
+         {/* NUEVO PROYECTOS */}
+         
+         <div className="ps-5 pt-0 pe-5 pb-5 m-0 skills-component">
+         <h3 id="projects" className="text-uppercase title text-center mb-4">proyectos</h3>
+         <div className="row">
+             <div className="col-md-6 mb-5 d-flex align-items-start">
+             <Projects title={'Portfolio'} description={'Mi propio portfolio'} url={''} urlRepo={'https://github.com/federicocaballero/portfolio.git'} img={portfolio} />
+             </div>
+             <div className="col-md-6 mb-5 d-flex align-items-start">
+             <Projects title={'Blog de café'} description={'Blog de sobre cafe con distintas paginas para navegar (Contacto, Cursos, Nosotros)'} url={'https://blogdecafetp.netlify.app/index.html'} urlRepo={'https://github.com/federicocaballero/blog-de-cafe.git'} img={blogCafe}/>
+             </div>
+         </div>
+         <div className="row gap-0">
+             <div className="col-md-6 mb-5 d-flex align-items-start">
+             <Projects title={'Travel Agency'} description={'Este pagina muestra los distintos destinos y ofertas de una agencia de viajes'} url={''} urlRepo={'https://github.com/federicocaballero/Travel-Agency-Home.git'} img={travelAgency} />
+             </div>
+             <div className="col-md-6 mb-5 d-flex align-items-start">
+             <Projects title={'Birreria'} description={'Este proyecto muestra las opciones y locales de un local de cerveza artesanal. Muestra las sucursales y marcas que sirven.'} url={'https://frontend-birreria.web.app/'} urlRepo={'https://github.com/federicocaballero/Birreria-FrontEnd.git'} img={birreria}/>
+             </div>
+         </div>
+         </div>
         </>
 
     )
